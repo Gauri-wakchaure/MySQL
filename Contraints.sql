e031c469659713045f94f1411d0323f489de9670
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-25 15:41:22 +0530

    Solve Practice Question-2

--- Contraints.sql
+++ Contraints.sql
@@ -2,7 +2,10 @@
 Constraints in following table:
 1. Primary key: Makes column unique and not null
 2. Not Null: This field not null must have to define 
-3. Unique: This must be unique */
+3. Unique: This must be unique
+4. foreign key: prevent action that would destroy link between tables
+5. Default: sets the default values of a column
+6. check: it can limit the values allowed in a column */
 
 create table Student(
     rollno int primary key, 
