014ba2b0d4b39cb5927dd1a4500fef326cb5caea
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-25 15:17:50 +0530

    Learn Primary and foreign key

---
+++ Keys.sql
@@ -0,0 +1,15 @@
+/* 
+1. Primary Key:
+- It is a column(set of columns)
+- Primary key must be unique always
+- Primary key must be not null
+- one table has only one primary key
+
+2. Foreign Key:
+- The column which is primary key is in another column called as 
+  foreign key.
+- There are multiple foreign keys.
+- It can be null
+- It can have duplicates not unique
+
+*/
