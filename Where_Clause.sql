d2ef37e6695679c7e7bdbff57cca207203d854b2
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-25 15:45:05 +0530

    learn Where Clause

---
+++ Where_Clause.sql
@@ -0,0 +1,31 @@
+/* Where Clause */
+
+create table student(
+    rollno int primary key, 
+    name varchar(50), 
+    marks int not null, 
+    grade varchar(1), 
+    city varchar(20));
+
+insert into student values
+(1, "Gauri", 92, "A", "Pune");
+
+insert into student values
+(2, "Priya", 87, "A", "Pune");
+
+insert into student values
+(3, "Ruhii", 80, "B", "Naashik");
+
+insert into student values
+(4, "Karan", 82, "B", "Mumbai");
+
+insert into student values
+(5, "Raj", 75, "B", "Nagpur");
+
+select * from student;
+
+select * from student where city = "Mumbai";
+
+select * from student where marks > 85;
+
+select * from student where marks < 80;
