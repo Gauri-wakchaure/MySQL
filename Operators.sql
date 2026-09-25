9544486754ac36b3f1da749722f41c99324ce67f
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-25 15:56:25 +0530

    Understand Operators

---
+++ Operators.sql
@@ -0,0 +1,35 @@
+/* 
+1. Arithmetic Operator:
+- Addition, Subtraction, Multiplication, Division, Modulus
+
+2. Comparison Operator:
+- Equal to, not Equal to, less than, grater than
+
+3. Logical Operator:
+- AND: Both conditions are true
+- OR: One condition Satisfy
+
+4. Bitwise Operators:
+- Bitwise AND, Bitwise OR
+
+5.Between Operation:
+- Selects specific range
+-EX: 80 to 90 range between data
+
+6. In Operator:
+- Matches any value in the list
+
+7. Not In:
+- Negate the given condition
+
+- Where clause
+- Checks one condition and show data
+
+-Limit Clause:
+sets an upper limit of data
+
+-Order By Clause
+- To sort in ascending or descending order
+- ASC and DESC
+
+*/
