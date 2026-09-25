1923e60fb804057ddb383577af7344db8e5de98c
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-25 14:52:43 +0530

    Types of SQL Commands

---
+++ SQL_Types.sql
@@ -0,0 +1,16 @@
+/*
+1. DDL(Data definition language)
+create, alter, rename, truncate, drop
+
+2. DQL(Data query language)
+select
+
+3. DML(Data manipulation language)
+insert, update, delete
+
+4. DCL(Data control language)
+grant and revoke permission to users
+
+5. TCL(Transaction control language)
+start transaction, commit, rollback
+*/
