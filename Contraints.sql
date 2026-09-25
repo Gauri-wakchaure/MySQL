d9afcf0780ee86f456e94f3926c7087bfb43f788
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-25 15:00:23 +0530

    Create Contraints

---
+++ Contraints.sql
@@ -0,0 +1,9 @@
+/*Contraint means define columns detail */
+
+create table Student(
+    rollno int primary key, 
+    name varchar(50), 
+    age int not null);
+
+insert into student values
+(101, "Gauri", 22);
