00ba8fb3c226a858ba9dca0f62c3d22062c9b68d
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-25 20:31:02 +0530

    Learn Truncate Command

---
+++ Truncate.sql
@@ -0,0 +1,12 @@
+/*
+Difference Truncate and Drop
+1. Truncate:
+- Used to delete only data from table structure 
+  of table remains same.
+
+2. Drop:
+- Drop is to to delete whole table
+  not only data.
+ */
+
+Truncate table Student;s
